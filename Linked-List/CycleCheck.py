fc0ad402c488ab51ca821892ad912e677158a761
AvrahamOhana <amiko617@gmail.com>
2022-11-03 22:30:39 +0200

    interview problem

---
+++ Linked-List/CycleCheck.py
@@ -0,0 +1,12 @@
+# check is there is a cycle in the kinked list
+def cycle_check(node):
+    marker1 = node
+    marker2 = node
+    
+    while marker2 != None and marker2.next_node != None:
+        marker1 = marker1.next_node
+        marker2 = marker2.mext_node.next_node
+        if marker2 == marker1:
+            return True
+        
+    return False
